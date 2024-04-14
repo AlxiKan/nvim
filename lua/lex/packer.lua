dd5092a68849947060a3cf101713c2d63ba23c09
lex <alexikan98@gmail.com>
2024-04-15 02:12:42 +0300

    added treesitter

--- lua/lex/packer.lua
+++ lua/lex/packer.lua
@@ -7,6 +7,7 @@ return require('packer').startup(function(use)
 	use {'nvim-telescope/telescope.nvim', tag = '0.1.6', requires = {{'nvim-lua/plenary.nvim'}}}
 	use 'ThePrimeagen/harpoon'
 	use 'tpope/vim-commentary'
+	use 'nvim-treesitter/nvim-treesitter'
 	use ({'rose-pine/neovim', as = 'rose-pine', config = function() vim.cmd('colorscheme rose-pine-moon') end})
 
 end)
