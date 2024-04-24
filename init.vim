7e90bba8e0db5883809b842ed93ba70b6c3c6892
lex <alexikan98@gmail.com>
2024-04-24 19:20:36 +0300

    changes

--- init.vim
+++ init.vim
@@ -3,6 +3,8 @@ lua require('lex.packer')
 set number
 set relativenumber
 
+set shiftwidth=4
+
 let mapleader = "\<Space>"
 
 nnoremap <leader>fd <cmd>Ex<cr>
--- lua/lex/packer.lua
+++ lua/lex/packer.lua
@@ -1,13 +1,11 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-
-	use 'wbthomason/packer.nvim'
-
-	use {'nvim-telescope/telescope.nvim', tag = '0.1.6', requires = {{'nvim-lua/plenary.nvim'}}}
-	use 'ThePrimeagen/harpoon'
-	use 'tpope/vim-commentary'
-	use 'nvim-treesitter/nvim-treesitter'
-	use ({'rose-pine/neovim', as = 'rose-pine', config = function() vim.cmd('colorscheme rose-pine-moon') end})
-
+    use 'wbthomason/packer.nvim'
+    use 'christoomey/vim-tmux-navigator'
+    use {'nvim-telescope/telescope.nvim', tag = '0.1.6', requires = {{'nvim-lua/plenary.nvim'}}}
+    use 'ThePrimeagen/harpoon'
+    use 'tpope/vim-commentary'
+    use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use ({'rose-pine/neovim', as = 'rose-pine', config = function() vim.cmd('colorscheme rose-pine-moon') end})
 end)
